7bf2461a1ada907cd12cc7add4b26d637e2fab2b
koyamal <74910401+koyamal@users.noreply.github.com>
2025-08-05 22:42:10 +0900

    fix

--- helloWorld/libs/use.js
+++ helloWorld/libs/use.js
@@ -1,4 +1,6 @@
-const libs = require("./look");
+// const libs = require("./look");
+const { libs } = require("./look");
+console.log(libs);
 libs.libs.look(() => {
   console.log('This is look');
 });
