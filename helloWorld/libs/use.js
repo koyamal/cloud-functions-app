c3596ee1e956cec735128268a44e559822a4b488
koyamal <74910401+koyamal@users.noreply.github.com>
2025-08-09 20:35:32 +0900

    create

--- helloWorld/libs/use.js
+++ helloWorld/libs/use.js
@@ -1,6 +1,8 @@
 const { libs } = require("./look");
 const { again } = require("./again");
-again(() => {});
+again(() => {
+  console.log("this")
+});
 console.log(libs);
 libs.look(() => {
   console.log('This is look from libs');
