915ed064d161d978648d1870bcdde762373e4941
koyamal <74910401+koyamal@users.noreply.github.com>
2025-08-11 22:31:29 +0900

    create

--- helloWorld/libs/back.js
+++ helloWorld/libs/back.js
@@ -1,7 +1,7 @@
 const back = (handleBack) => {
   console.log('back start');
   handleBack && handleBack();
-  handleBack && console.log('Hello This is World');
+  handleBack? console.log('Hello This is World'): console.log('No World');
   console.log('back done');
 }
 
